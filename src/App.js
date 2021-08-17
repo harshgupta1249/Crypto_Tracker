3043db2e7bd58aeb87a96a354900adc0ba2f5dec
Harsh <harshgupta1249@gmail.com>
2021-08-18 00:00:14 +0530

    axios package installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import './App.css';
+
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(()=>{
+
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+      console.log(res.data);
+    })
+    .catch(error => console.log(error))
+  }, []);
+
   return (
     <div className="App">
-      <h1>API's</h1>
+      <h1>API'z</h1>
     </div>
   );
 }
