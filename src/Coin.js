0a65a1827765937b9bff1c0bf5a24228734a4ec2
Harsh <harshgupta1249@gmail.com>
2021-08-18 17:18:39 +0530

    coins data fetched

---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+const Coin = ({name, image, symbol, volume, price, priceChange, marketcap}) => {
+    return ( 
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto" />
+                    <div className="coin-name">{name}</div>
+                    <div className="coin-symbol">{symbol}</div>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">{price}</p>
+                    <p className="coin-volume">{volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                    ): (
+                        <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+                    )}
+                    <p className="coin-marketcap">
+                        Mkt Cap: {marketcap.toLocaleString()}
+                    </p>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Coin;
